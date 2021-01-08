28b9ef4d945d945d27852f14fcdd4e7b68d578d0
gaurankit <ankigaur@gmail.com>
2021-01-08 16:04:14 -0600

    Added RecommendationService mock implementation and create travel product models.

---
+++ Portal/src/app/Model/Flight.ts
@@ -0,0 +1,13 @@
+export class Flight {
+  number: number;
+  airlineName: string;
+  startDateTime:Date;
+  endDateTime:Date;
+  duration:string;
+  Price:number;
+  stops:number;
+  refundable:boolean;
+  freeChange:boolean;
+  rank:number;
+  logo:string;
+}
