635f7e72a2dc176c29b78d7552e7355bca8e3d92
mansipatel05 <mansipatel0587@gmail.com>
2021-01-08 18:33:03 -0600

    Resolving merge conflict

--- Portal/src/app/app.module.ts
+++ Portal/src/app/app.module.ts
@@ -4,20 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { ToursListComponent } from './tours-list/tours-list.component';
-=======
 import { SearchComponent } from './home/search.component'
->>>>>>> 48a1e5d0607834bbe2902fbe0996d330900ea07b
+
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    ToursListComponent
-=======
+    ToursListComponent,
     SearchComponent
->>>>>>> 48a1e5d0607834bbe2902fbe0996d330900ea07b
   ],
   imports: [
     BrowserModule,
